98ce859e752c9d25a79b2cd438f69e182e0975a4
CCDM25 <tono.astorga2737@gmail.com>
2017-05-24 15:05:36 -0600

    eventos.php-pendiente,datos.php valida en prueba

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,4 +1,22 @@
 var iniciaApp = function(){
 	alert("Hola App");
+	var entrar=function(){
+		alert($("#txtUsuario").val());
+		alert($("#txtClave").val());
+	}
+	//Sección de declaración de eventos
+	var teclaUsuario=function(tecla){
+		if(tecla.which==13){
+			$("#txtClave").focus();
+		}
+	}
+	var teclaClave=function(tecla){
+		if(tecla.which==13){
+			entrar();
+		}
+	}
+	$("#btnEntrar").on("click",entrar);
+	$("#txtUsuario").on("keypress",teclaUsuario);
+	$("#txtClave").on("keypress",teclaClave);
 }
-$(document).on("ready",iniciaApp);
+$(document).ready(iniciaApp);
